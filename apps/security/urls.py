95bc739b32811c4fb1c2e80008c6dd1ab34ffcf0
luis030794 <luis.tobar.94@hotmail.com>
2015-08-28 15:04:17 -0500

    Revert "asd"
    This reverts commit c0df58bd2aacde15da84a9d9be18e73d5a2c2cb5.

--- apps/security/urls.py
+++
@@ -1,8 +0,0 @@
-from   django.conf.urls import patterns,url,include
-from django import *
-from django.template import *
-from SIGIF.apps.security.views import *
-
-urlpatterns= patterns('SIGIF.apps.security.views',
-                      url(r'^index$','index_view',name="index"),
-                      )
--- static/js/bb-alert-callback.js
+++
@@ -1,7 +0,0 @@
-bootbox.alert({
-   title: 'Title',
-   message: 'Test',
-   callback: function(){
-      Example.show("Hello world callback");
-   }
-});
